ada5ab5f4b2a314ea5394405b5884375c815e389
lucas castro <lucas_o__castro@hotmail.com>
2024-07-14 23:39:51 -0300

    Basic mod config support

---
+++ StardewBetterFrog/ModConfig.cs
@@ -0,0 +1,19 @@
+﻿using StardewModdingAPI;
+
+namespace StardewBetterFrog;
+
+public sealed class ModConfig
+{
+    public bool CountForBounty { get; set; }
+    
+    public void RegisterConfig(IManifest manifest, IGenericModConfigMenuApi configMenu)
+    {
+        configMenu.AddBoolOption(
+            mod: manifest,
+            name: () => "Count for guild bounty",
+            tooltip: () => "If checked, monsters a frog swallows will count towards the adventure guild bounty board.",
+            getValue: () => CountForBounty,
+            setValue: value => CountForBounty = value
+        );
+    }
+}
--- StardewBetterFrog/ModEntry.cs
+++ StardewBetterFrog/ModEntry.cs
@@ -1,10 +1,29 @@
 ﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
 
 namespace StardewBetterFrog;
 
-public class ModEntry : Mod
+public sealed class ModEntry : Mod
 {
+    private ModConfig _config = new();
+    
     public override void Entry(IModHelper helper)
     {
+        //Setup config
+        _config = helper.ReadConfig<ModConfig>();
+        helper.Events.GameLoop.GameLaunched += SetupConfigMenu;
+    }
+
+    private void SetupConfigMenu(object? _, GameLaunchedEventArgs e)
+    {
+        var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        if (configMenu == null) return;
+        
+        configMenu.Register(
+            mod: ModManifest,
+            reset: () => _config = new ModConfig(),
+            save: () => Helper.WriteConfig(_config)
+        );
+        _config.RegisterConfig(ModManifest, configMenu);
     }
 }
